19a7871980479926ca62054cbcf6fd3bd3e39998
QCoQCo <kbw7700@gmail.com>
2025-08-21 17:22:13 +0900

    pull REQUIRED 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { Header, Footer } from './components/common';
+import { HomePage } from './components/pages';
 import './App.css';
 
 const Layout = () => {
     return (
         <div id='Layout'>
-            <Outlet />
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </div>
     );
 };
@@ -13,7 +19,9 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Outlet />}></Route>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+    return <div id='HomePage'>HomePage</div>;
+};
+
+export default HomePage;
