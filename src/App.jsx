16cff2c36db8c5f516a4643360560d47e5e9ca54
QCoQCo <kbw7700@gmail.com>
2025-08-21 16:46:57 +0900

    pull REQUIRED 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 
+const Layout = () => {
+    return (
+        <div id='Layout'>
+            <Outlet />
+        </div>
+    );
+};
+
 function App() {
-    return <div id='App'>App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Outlet />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
