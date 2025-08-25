b216ac6be1fbc2d0673d71a796979790f8530975
devbhkim0707 <devbhkim0707@gmail.com>
2025-08-25 18:24:01 +0900

    fix: resolved issue creating grid items with same key refactor: scss nesting refactor

--- src/components/pages/MyPage.jsx
+++ src/components/pages/MyPage.jsx
@@ -11,85 +11,102 @@ const initialLayout = [
 ];
 
 const MyPage = () => {
+    // layout 상태를 useState로 관리하여 동적 변경이 가능하게 합니다.
     const [layout, setLayout] = useState(initialLayout);
 
+    // 드롭된 아이템의 내용에 대한 카운터를 useState로 관리합니다.
     const [newItemCounter, setNewItemCounter] = useState(0);
 
     const onDrop = (currentLayout, droppedItemProps) => {
-        const newId = `new-item-${newItemCounter}`;
+        // 새로운 아이템에 대한 고유한 ID를 생성합니다.
+        setNewItemCounter((prevCounter) => {
+            const newId = `new-item-${prevCounter}`;
+            const newItem = {
+                i: newId,
+                x: droppedItemProps.x,
+                y: droppedItemProps.y,
+                w: 1, // 드롭 시 새로운 아이템의 기본 크기를 지정
+                h: 1, // 드롭 시 새로운 아이템의 기본 크기를 지정
+            };
+            // layout 상태 업데이트는 prevLayout을 사용
+            setLayout((prevLayout) => [...prevLayout, newItem]);
 
-        const newItem = {
-            i: newId,
-            x: droppedItemProps.x,
-            y: droppedItemProps.y,
-            w: droppedItemProps.w,
-            h: droppedItemProps.h,
-        };
+            console.log(newItem);
+            return prevCounter + 1;
+        });
 
-        setLayout([...currentLayout, newItem]);
+        console.log('레이아웃', layout);
+        console.log('아이템', droppedItemProps);
 
+        // 카운터를 증가시켜 다음 아이템에 고유한 ID를 부여합니다.
         setNewItemCounter((prevCounter) => prevCounter + 1);
     };
 
+    // 레이아웃 데이터를 기반으로 아이템을 렌더링하는 함수입니다.
     const renderGridItems = () => {
         return layout.map((item) => {
-            if (item.i.startsWith('new-item')) {
-                return (
-                    <div key={item.i} className='new-grid-item'>
-                        <div className='grid-item-text'>New Item {newItemCounter}</div>
-                    </div>
-                );
-            } else {
-                return (
-                    <div className={item.i} key={item.i}>
-                        <div className='grid-item-text'>
-                            {item.i.replace(/-/g, ' ').toUpperCase()}
-                        </div>
-                    </div>
-                );
-            }
+            const isNewItem = item.i.startsWith('new-item');
+            const itemText = isNewItem
+                ? `New Item ${item.i.split('-').pop()}`
+                : item.i.replace(/-/g, ' ').toUpperCase();
+
+            return (
+                <div className={isNewItem ? 'new-grid-item' : item.i} key={item.i}>
+                    <div className='grid-item-text'>{itemText}</div>
+                </div>
+            );
         });
     };
 
     return (
         <div id='MyPage'>
-            <div className='floating-droppables-ui'>
-                <div
-                    className='droppable-element'
-                    draggable={true}
-                    unselectable='on'
-                    onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
-                >
-                    Drag Element1
+            <div className='user-info-area'>
+                <div className='profile-photo'>
+                    <img id='user-profile-photo' src='' alt='' />
                 </div>
-                <div
-                    className='droppable-element'
-                    draggable={true}
-                    unselectable='on'
-                    onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
-                >
-                    Drag Element2
-                </div>
-                <div
-                    className='droppable-element'
-                    draggable={true}
-                    unselectable='on'
-                    onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
-                >
-                    Drag Element3
+                <div className='user-introduction'>
+                    <textarea></textarea>
                 </div>
             </div>
-            <ReactGridLayout
-                layout={layout}
-                cols={5}
-                rowHeight={273}
-                width={1000}
-                onDrop={onDrop}
-                isDroppable={true}
-                compactType={'vertical'}
-            >
-                {renderGridItems()}
-            </ReactGridLayout>
+            <div className='blog-area'>
+                <div className='floating-droppables-ui'>
+                    <div
+                        className='droppable-element'
+                        draggable={true}
+                        unselectable='on'
+                        onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
+                    >
+                        Drag Element1
+                    </div>
+                    <div
+                        className='droppable-element'
+                        draggable={true}
+                        unselectable='on'
+                        onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
+                    >
+                        Drag Element2
+                    </div>
+                    <div
+                        className='droppable-element'
+                        draggable={true}
+                        unselectable='on'
+                        onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
+                    >
+                        Drag Element3
+                    </div>
+                </div>
+                <ReactGridLayout
+                    layout={layout}
+                    cols={5}
+                    rowHeight={170}
+                    width={900}
+                    onDrop={onDrop}
+                    isDroppable={true}
+                    compactType={'vertical'}
+                >
+                    {renderGridItems()}
+                </ReactGridLayout>
+            </div>
         </div>
     );
 };
