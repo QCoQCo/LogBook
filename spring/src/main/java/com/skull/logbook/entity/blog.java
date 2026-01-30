b20976b0709a15c2dac4b74ddd67e942f00fd71b
devbhkim0707 <devbhkim0707@gmail.com>
2026-01-30 16:32:44 +0900

    feat) implement blog entity with JPA annotations and layout field

--- spring/src/main/java/com/skull/logbook/entity/blog.java
+++ spring/src/main/java/com/skull/logbook/entity/blog.java
@@ -1,2 +1,17 @@
-package com.skull.logbook.entity;public class blog {
+package com.skull.logbook.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class blog extends BaseDeletedEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String layout;
 }
