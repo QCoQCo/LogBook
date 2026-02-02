51f69d8aaf0e0da350097b71a97513ef937dfcfa
KTH <proxogus@naver.com>
2026-02-02 15:33:19 +0900

    refactor: rename `auth_sessions` table to `authSessions` in `AuthSession` entity.

--- spring/src/main/java/com/skull/logbook/entity/AuthSession.java
+++ spring/src/main/java/com/skull/logbook/entity/AuthSession.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@Table(name = "auth_sessions")
+@Table(name = "authSessions")
 @EntityListeners(AuditingEntityListener.class)
 public class AuthSession extends BaseUpdatedEntity {
 
